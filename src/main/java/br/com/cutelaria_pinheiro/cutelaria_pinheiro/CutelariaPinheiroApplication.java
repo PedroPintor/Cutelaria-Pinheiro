af0cd0521f8c2309dd113d1747b7c7871cea7346
202309191482 <202309191482@MININT-3J7H85B.EDUC.ACAD>
2024-11-01 10:51:29 -0300

    ambiente de desenvolvimento

---
+++ src/main/java/br/com/cutelaria_pinheiro/cutelaria_pinheiro/CutelariaPinheiroApplication.java
@@ -0,0 +1,13 @@
+package br.com.cutelaria_pinheiro.cutelaria_pinheiro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CutelariaPinheiroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CutelariaPinheiroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/cutelaria_pinheiro/cutelaria_pinheiro/CutelariaPinheiroApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.cutelaria_pinheiro.cutelaria_pinheiro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CutelariaPinheiroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
