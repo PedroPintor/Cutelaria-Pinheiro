a5da33db78a3f90b8e1923699d292e883d102e63
PedroPintor <202309191482@alunos.ibmec.edu.br>
2024-11-12 14:20:47 -0300

    :beers: sucesso: tela da vitrine + informaçoes do produto

--- src/main/java/br/com/cutelaria_pinheiro/cutelaria_pinheiro/controller/CutelariaController.java
+++ src/main/java/br/com/cutelaria_pinheiro/cutelaria_pinheiro/controller/CutelariaController.java
@@ -1,21 +1,13 @@
 package br.com.cutelaria_pinheiro.cutelaria_pinheiro.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import br.com.cutelaria_pinheiro.cutelaria_pinheiro.service.ProdutoService;
 
 @Controller
 @RequestMapping("/cutelaria-pinheiro")
 public class CutelariaController {
-
-    @Autowired
-    private ProdutoService produtoService;
-
-    
 
     // pagina principal
     @GetMapping("")
